d717b6025075b79869af3554a046ffcabeea186b
David Xu <dxaviud@gmail.com>
2021-02-18 22:26:13 -0500

    pep8 format

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import Tk
 
+
 class ClipboardCopier:
 
     def __init__(self):
@@ -14,9 +15,10 @@ class ClipboardCopier:
     def destroy(self):
         self.root.destroy()
 
+
 class ELanguageTranslator:
 
-    def __init__(self, to_convert = "aeiou"):
+    def __init__(self, to_convert="aeiou"):
         self.to_convert = to_convert.lower()
 
     def text_contains_character(self, text, character):
@@ -29,12 +31,13 @@ class ELanguageTranslator:
             return 'E'
         return original_character
 
-    def translate(self, text = "e language"):
+    def translate(self, text="e language"):
         translated_text = []
         for character in text:
             translated_character = self.get_translated_character(character)
             translated_text.append(translated_character)
         return "".join(translated_text)
+
 
 class ELanguageTranslatorProgram:
 
@@ -47,18 +50,21 @@ class ELanguageTranslatorProgram:
 
     def validate_user_response(self):
         while self.user_response.lower() != 'y' and self.user_response.lower() != 'n':
-            self.user_response = input("Unrecognized input. Continue translating? (y/n): ")
+            self.user_response = input(
+                "Unrecognized input. Continue translating? (y/n): ")
 
     def run(self):
         while True:
             self.user_text = input("Enter text to convert to E language:\n\n")
             print()
-            self.translated_text = self.language_translator.translate(self.user_text)
+            self.translated_text = self.language_translator.translate(
+                self.user_text)
             print("Translated text:\n")
             print(self.translated_text)
             self.clipboard_copier.copy_text_to_clipboard(self.translated_text)
             print()
-            self.user_response = input("Translated text has been copied to clipboard. Continue translating? (y/n): ")
+            self.user_response = input(
+                "Translated text has been copied to clipboard. Continue translating? (y/n): ")
             self.validate_user_response()
             if self.user_response.lower() == 'n':
                 self.clipboard_copier.destroy()
